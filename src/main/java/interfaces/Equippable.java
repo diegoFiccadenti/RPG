35046fd6bfa45d191b239d5a206573311ff42cfb
Diego Ficcadenti <diego.ficcadenti@studenti.unicam.it>
2026-05-03 19:06:41 +0200

    Added class Weapon and Interface Equippable

---
+++ src/main/java/interfaces/Equippable.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface Equippable {
+
+    public void equip();
+
+    public void unEquip();
+}
---
+++ src/main/java/model/Weapon.java
@@ -0,0 +1,37 @@
+package model;
+
+import interfaces.Equippable;
+
+public class Weapon extends Item implements Equippable {
+
+    boolean equipped;
+    int magicalDamage;
+    int physicalDamage;
+
+    public Weapon(String name, String description, int physicalDamage, int magicalDamage) {
+        super(name, description);
+        equipped = false;
+        this.physicalDamage = physicalDamage;
+        this.magicalDamage = magicalDamage;
+    }
+
+    public boolean isEquipped() {
+        return equipped;
+    }
+
+    public int getMagicalDamage() {
+        return magicalDamage;
+    }
+
+    public int getPhysicalDamage() {
+        return physicalDamage;
+    }
+
+    public void equip() {
+        equipped = true;
+    }
+
+    public void unEquip() {
+        equipped = false;
+    }
+}
