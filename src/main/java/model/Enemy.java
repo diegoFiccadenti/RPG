ad83baf3c7058ea767580ef9fb371b232cf575ad
Diego Ficcadenti <diego.ficcadenti@studenti.unicam.it>
2026-05-03 16:23:14 +0200

    Added Interface Lootable and class Enemy

---
+++ src/main/java/model/Enemy.java
@@ -0,0 +1,15 @@
+package model;
+
+import interfaces.Fighter;
+import interfaces.Lootable;
+
+public class Enemy extends Character implements Fighter, Lootable {
+
+    public Enemy (String name) {
+        super(name);
+    }
+
+    public void attack(){}
+
+    public void takeDamage() {}
+}
