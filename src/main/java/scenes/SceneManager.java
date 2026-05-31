78887bf64de51b1e8ab87392446b18e277a0e1a1
Diego Ficcadenti <diego.ficcadenti@studenti.unicam.it>
2026-05-31 17:07:58 +0200

    Scene caching added in SceneManager.java

--- src/main/java/scenes/SceneManager.java
+++ src/main/java/scenes/SceneManager.java
@@ -10,6 +10,10 @@ public class SceneManager {
     public static final int SCREEN_WIDTH = 640;
     public static final int SCREEN_HEIGHT = 480;
 
+    private final MyScene mainMenuScene;
+    private final MyScene tutorialScene;
+    private final MyScene gameScene;
+
     public enum SceneType {
         MAIN_MENU,
         TUTORIAL,
@@ -18,21 +22,26 @@ public class SceneManager {
 
     public SceneManager(Stage stage) {
         this.stage = stage;
+
         stage.setTitle("MyRPG");
         stage.setResizable(false);
+
+        mainMenuScene = new MainMenu(this);
+        tutorialScene = new Tutorial(this);
+        gameScene = new Game(this);
     }
 
     public void switchScene(SceneType scene) {
 
         Scene newScene = null;
         if (scene == SceneType.MAIN_MENU) {
-            newScene = new MainMenu(this).getScene();
+            newScene = mainMenuScene.getScene();
         }
         else if (scene == SceneType.TUTORIAL) {
-            newScene = new Tutorial(this).getScene();
+            newScene = tutorialScene.getScene();
         }
         else if (scene == SceneType.GAME) {
-            newScene = new Game(this).getScene();
+            newScene = gameScene.getScene();
         }
         else throw new IllegalArgumentException("Invalid scene type");
 
