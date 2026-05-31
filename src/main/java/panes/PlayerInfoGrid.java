717508a25b260d5976efc2a2883d0ad81541be04
Diego Ficcadenti <diego.ficcadenti@studenti.unicam.it>
2026-05-31 21:23:19 +0200

    Created class PlayerInfoGrid.java.

---
+++ src/main/java/panes/PlayerInfoGrid.java
@@ -0,0 +1,93 @@
+package panes;
+
+import entities.Player;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+public class PlayerInfoGrid {
+
+    private final GridPane grid;
+
+    private final Label playerName;
+    private final Label playerLevel;
+    private final Label playerCoins;
+
+    private int currentHP;
+    private int maxHP;
+    private final Label HPLabel;
+    private final Label HPValue;
+    private final ProgressBar HPBar;
+    private final HBox HP_HBox;
+
+    private int currentMP;
+    private int maxMP;
+    private final Label MPLabel;
+    private final Label MPValue;
+    private final ProgressBar MPBar;
+    private final HBox MP_HBox;
+
+    public PlayerInfoGrid(Player player) {
+
+        grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(100);
+        grid.setVgap(10);
+
+        this.playerName = new Label();
+        this.playerLevel = new Label();
+        this.playerCoins = new Label();
+
+        this.HPLabel = new Label("HP");
+        this.HPValue = new Label(currentHP + " / " + maxHP);
+        this.HPBar = new ProgressBar();
+        this.HP_HBox = new HBox();
+        HP_HBox.setSpacing(10);
+        HP_HBox.setAlignment(Pos.CENTER_LEFT);
+
+        this.MPLabel = new Label("MP");
+        this.MPValue = new Label(currentMP + " / " + maxMP);
+        this.MPBar = new ProgressBar();
+        this.MP_HBox = new HBox();
+        MP_HBox.setSpacing(10);
+        MP_HBox.setAlignment(Pos.CENTER_LEFT);
+
+        updateGrid(player);
+    }
+
+    public GridPane getGrid() {return grid;}
+
+    public void updateGrid(Player player) {
+
+        this.playerName.setText("Name: " + player.getName());
+        this.playerLevel.setText("Level: " + player.getLevel());
+        this.playerCoins.setText("Coins: " + player.getCoins().getCurrentValue());
+
+        this.currentHP = player.getCombatStats().getHP().getCurrentValue();
+        this.maxHP = player.getCombatStats().getHP().getMaxValue();
+        this.HPValue.setText(currentHP + " / " + maxHP);
+        this.HPBar.setProgress((double) currentHP / maxHP);
+        this.HP_HBox.getChildren().clear();
+        this.HP_HBox.getChildren().addAll(HPLabel, HPBar, HPValue);
+
+        this.currentMP = player.getCombatStats().getMP().getCurrentValue();
+        this.maxMP = player.getCombatStats().getMP().getMaxValue();
+        this.MPValue.setText(currentMP + " / " + maxMP);
+        this.MPBar.setProgress((double) currentMP / maxMP);
+        this.MP_HBox.getChildren().clear();
+        this.MP_HBox.getChildren().addAll(MPLabel, MPBar, MPValue);
+
+        addValuesToGrid();
+    }
+
+    private void addValuesToGrid() {
+        grid.getChildren().clear();
+        grid.add(playerName, 0,0);
+        grid.add(playerLevel, 0,1);
+        grid.add(playerCoins, 0,2);
+        grid.add(HP_HBox, 1,0);
+        grid.add(MP_HBox, 1,1);
+    }
+}
--- src/main/java/scenes/Game.java
+++ src/main/java/scenes/Game.java
@@ -2,22 +2,25 @@ package scenes;
 
 import entities.Player;
 import data_structures.Inventory;
-import javafx.geometry.Pos;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Label;
+import panes.PlayerInfoGrid;
 
 public class Game implements MyScene {
 
+    // TODO: separare in due oggetti distinti la creazione di InfoGrid e MainPane
+
     // TODO: implementare creazione personaggio e sistema di permanenza dati
+
     // creazione player temporanea:
     private Player player = new Player(
             "NomeDiProva",
             new Inventory()
     );
+
+    private PlayerInfoGrid playerInfoGrid;
 
     private final Scene scene;
 
@@ -25,7 +28,8 @@ public class Game implements MyScene {
 
         Pane mainPane = createMainPane(sceneManager);
 
-        GridPane infoGrid = createInformationGrid();
+        this.playerInfoGrid = new PlayerInfoGrid(player);
+        GridPane infoGrid = playerInfoGrid.getGrid();
 
         BorderPane root = new BorderPane();
         root.setCenter(mainPane);
@@ -42,58 +46,19 @@ public class Game implements MyScene {
         pane.setBackground(new Background((new BackgroundFill(Color.DARKGRAY, null, null))));
 
         Button exit = new Button("Exit");
+        exit.setPrefSize(100, 100);
         exit.setOnAction(e -> {
             sceneManager.switchScene(SceneManager.SceneType.MAIN_MENU);
         });
 
-        pane.getChildren().add(exit);
+        Button takeDamage = new Button("Take Damage");
+        takeDamage.setOnAction(e -> {
+            player.getCombatStats().getHP().decreaseCurrent(10);
+            playerInfoGrid.updateGrid(player);
+        });
+
+        pane.getChildren().addAll(exit, takeDamage);
 
         return pane;
-    }
-
-    private GridPane createInformationGrid() {
-
-        Label playerName = new Label("Name: " + player.getName());
-        Label playerLevel = new Label("Level: " + player.getLevel());
-        Label playerCoins = new Label("Coins: " + player.getCoins().getCurrentValue());
-
-        int currentHP = player.getCombatStats().getHP().getCurrentValue();
-        int maxHP = player.getCombatStats().getHP().getMaxValue();
-        int currentMP = player.getCombatStats().getMP().getCurrentValue();
-        int maxMP = player.getCombatStats().getMP().getMaxValue();
-
-        Label HP = new Label("HP");
-        Label HPValue = new Label(currentHP + " / " + maxHP);
-        ProgressBar HPBar = new ProgressBar();
-        HPBar.setProgress((double) currentHP / maxHP);
-        HBox HP_HBox = createProgressBarWithExplicitValues(HP, HPBar, HPValue);
-
-        Label MP = new Label("MP");
-        Label MPValue = new Label(currentMP + " / " + maxMP);
-        ProgressBar MPBar = new ProgressBar();
-        MPBar.setProgress((double) currentMP / maxMP);
-        HBox MP_HBox = createProgressBarWithExplicitValues(MP, MPBar, MPValue);
-
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(100);
-        grid.setVgap(10);
-
-        grid.add(playerName, 0,0);
-        grid.add(playerLevel, 0,1);
-        grid.add(playerCoins, 0,2);
-        grid.add(HP_HBox, 1,0);
-        grid.add(MP_HBox, 1,1);
-
-        return grid;
-    }
-
-    private HBox createProgressBarWithExplicitValues(Label barName, ProgressBar bar, Label explicitValue) {
-        HBox hBox = new HBox();
-        hBox.setSpacing(10);
-        hBox.setAlignment(Pos.CENTER_LEFT);
-        hBox.getChildren().addAll(barName, bar, explicitValue);
-
-        return hBox;
     }
 }
