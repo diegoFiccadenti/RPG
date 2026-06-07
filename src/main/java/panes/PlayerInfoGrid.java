2b1455887a86ed66799786867b57d33201ed60e1
Diego Ficcadenti <diego.ficcadenti@studenti.unicam.it>
2026-06-07 08:11:12 +0200

    PlayerInfoGrid.java redundancies eliminated, now is more efficient

--- src/main/java/panes/PlayerInfoGrid.java
+++ src/main/java/panes/PlayerInfoGrid.java
@@ -18,16 +18,10 @@ public class PlayerInfoGrid implements PlayerObserver {
     private final Label playerLevel;
     private final Label playerCoins;
 
-    private int currentHP;
-    private int maxHP;
-    private final Label HPLabel;
     private final Label HPValue;
     private final ProgressBar HPBar;
     private final HBox HP_HBox;
 
-    private int currentMP;
-    private int maxMP;
-    private final Label MPLabel;
     private final Label MPValue;
     private final ProgressBar MPBar;
     private final HBox MP_HBox;
@@ -46,14 +40,12 @@ public class PlayerInfoGrid implements PlayerObserver {
         this.playerLevel = new Label();
         this.playerCoins = new Label();
 
-        this.HPLabel = new Label("HP");
         this.HPValue = new Label();
         this.HPBar = new ProgressBar();
         this.HP_HBox = new HBox();
         HP_HBox.setSpacing(10);
         HP_HBox.setAlignment(Pos.CENTER_LEFT);
 
-        this.MPLabel = new Label("MP");
         this.MPValue = new Label();
         this.MPBar = new ProgressBar();
         this.MP_HBox = new HBox();
@@ -61,6 +53,9 @@ public class PlayerInfoGrid implements PlayerObserver {
         MP_HBox.setAlignment(Pos.CENTER_LEFT);
 
         refreshData(player);
+        this.HP_HBox.getChildren().addAll(new Label("HP"), HPBar, HPValue);
+        this.MP_HBox.getChildren().addAll(new Label("MP"), MPBar, MPValue);
+        addValuesToGrid();
     }
 
     public GridPane getGrid() {return grid;}
@@ -71,21 +66,15 @@ public class PlayerInfoGrid implements PlayerObserver {
         this.playerLevel.setText("Level: " + player.getLevel());
         this.playerCoins.setText("Coins: " + player.getCoins().getCurrentValue());
 
-        this.currentHP = player.getCombatStats().getHP().getCurrentValue();
-        this.maxHP = player.getCombatStats().getHP().getMaxValue();
+        int currentHP = player.getCombatStats().getHP().getCurrentValue();
+        int maxHP = player.getCombatStats().getHP().getMaxValue();
         this.HPValue.setText(currentHP + " / " + maxHP);
         this.HPBar.setProgress((double) currentHP / maxHP);
-        this.HP_HBox.getChildren().clear();
-        this.HP_HBox.getChildren().addAll(HPLabel, HPBar, HPValue);
 
-        this.currentMP = player.getCombatStats().getMP().getCurrentValue();
-        this.maxMP = player.getCombatStats().getMP().getMaxValue();
+        int currentMP = player.getCombatStats().getMP().getCurrentValue();
+        int maxMP = player.getCombatStats().getMP().getMaxValue();
         this.MPValue.setText(currentMP + " / " + maxMP);
         this.MPBar.setProgress((double) currentMP / maxMP);
-        this.MP_HBox.getChildren().clear();
-        this.MP_HBox.getChildren().addAll(MPLabel, MPBar, MPValue);
-
-        addValuesToGrid();
     }
 
     private void addValuesToGrid() {
