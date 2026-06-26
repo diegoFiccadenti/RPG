05b8c4890cf23823e035215ec5cbfbe5149e7c86
Diego Ficcadenti <diego.ficcadenti@studenti.unicam.it>
2026-06-26 14:29:57 +0200

    Project renamed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 // TODO 01: risolvere conflitti e bug con la permanenza dei dati (in particolare danno problemi gli items equipaggiati + attacchi imparati)
-// TODO 02: risolvere problemi di bilanciamento del gioco (opzionale)
-// TODO 03: fare in modo che non si possa iniziare un combattimento senza attacchi equipaggiati
-// TODO 04: assegnare 2 attacchi di base al Player già alla creazione (prima risolvi problemi di persistenza)
-// TODO 05: controllare le classi una ad una per eventuali metodi inutilizzati o TODO lasciati in sospeso
+// TODO 02: assegnare 2 attacchi di base al Player già alla creazione (prima risolvi problemi di persistenza)
+// TODO 03: controllare le classi una ad una per eventuali metodi inutilizzati o TODO lasciati in sospeso
 
 public class Main extends Application {
 
